674e0028080928ae680fc32c5da7f464dae30e3b
bambamayo <ayobamiagunroye@gmail.com>
2020-05-22 23:04:37 +0100

    finished review list page

--- src/reviews/pages/ReviewDetails.js
+++ src/reviews/pages/ReviewDetails.js
@@ -4,9 +4,19 @@ import { useParams } from "react-router-dom";
 const ReviewDetails = () => {
   let { name } = useParams();
   return (
-    <div>
-      <h1>i am showing the review details for {name} </h1>
-    </div>
+    <section className="review-details">
+      <div className="review-details__cont">
+        <h2 className="review-details__header">{`category/${name}`}</h2>
+        <button className="review-details__editbtn">edit</button>
+      </div>
+      <div className="review-details__cont">
+        <h3 className="review-details__amount">
+          <span className="review-details__amount--pri">Number of reviews</span>
+          <span className="review-details__amount--sec">3</span>
+        </h3>
+        <button className="review-details__addtolistbtn">add to list</button>
+      </div>
+    </section>
   );
 };
 
