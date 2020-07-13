fa8ad4679ed21ffb7488ad3da3e2c173b5e1988e
bambamayo <ayobamiagunroye@gmail.com>
2020-07-13 04:43:59 +0100

    added action creators and action types to redux, login page now use redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,10 +3,12 @@ import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import authReducer from "./reducers/authReducer";
 import reviewsReducer from "./reducers/reviewsReducer";
+import modalReducer from "./reducers/modalReducer";
 
 const reducer = combineReducers({
   auth: authReducer,
   reviews: reviewsReducer,
+  showModal: modalReducer,
 });
 
 const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
